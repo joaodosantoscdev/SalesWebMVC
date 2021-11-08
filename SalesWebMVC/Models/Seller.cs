6fdd06e20a9cce8c210ebbdf468b17356c44b0cf
João Vítor Carvalho <mistingerr@gmail.com>
2021-11-08 20:11:25 -0300

    adding validation/client-and-server-side

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -9,19 +9,25 @@ namespace SalesWebMVC.Models
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and  {1}")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
         [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         public string Email { get; set; }
-
+        [Required(ErrorMessage = "{0} is requered")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+        [Required(ErrorMessage = "{0} is required")]
         [Display(Name = "Base Salary")]
+        [Range(100.0, 500000.0, ErrorMessage = "{0} must be from {1} and {2}")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
-
+        [Required(ErrorMessage = "{0} is required")]
         [Display(Name = "Department")]
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
